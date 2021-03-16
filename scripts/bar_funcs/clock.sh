2b3fc280002ce88643ff29025ea521530f2ad93d
yum <savvyyum7119@protonmail.ch>
2021-03-16 01:42:14 -0400

    Update clock.sh

--- scripts/bar_funcs/clock.sh
+++ scripts/bar_funcs/clock.sh
@@ -1,25 +1,21 @@
 #!/bin/sh
 
-clock () {
-    clock=$(date '+%I')
+clock=$(date '+%I')
 
-    case "$clock" in
-        "00") icon="" ;;
-        "01") icon="" ;;
-        "02") icon="" ;;
-        "03") icon="" ;;
-        "04") icon="" ;;
-        "05") icon="" ;;
-        "06") icon="" ;;
-        "07") icon="" ;;
-        "08") icon="" ;;
-        "09") icon="" ;;
-        "10") icon="" ;;
-        "11") icon="" ;;
-        "12") icon="" ;;
-    esac
+case "$clock" in
+    "00") icon="" ;;
+    "01") icon="" ;;
+    "02") icon="" ;;
+    "03") icon="" ;;
+    "04") icon="" ;;
+    "05") icon="" ;;
+    "06") icon="" ;;
+    "07") icon="" ;;
+    "08") icon="" ;;
+    "09") icon="" ;;
+    "10") icon="" ;;
+    "11") icon="" ;;
+    "12") icon="" ;;
+esac
 
-    echo "$(date "+%b %d %Y (%a) $icon %I:%M%p")"
-}
-
-clock
+echo "$(date "+%b %d %Y (%a) $icon %I:%M%p")"
