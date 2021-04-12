4b9ec67e0ecdcf209db456b70e1c366c97e801c1
yum <savvyyum7119@protonmail.ch>
2021-04-12 12:48:38 -0400

    Delete scripts directory

--- scripts/bar_funcs/vpn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-state="$(ip a | grep tun0 | grep inet | wc -l)"
-    
-if [ $state = 1 ]; then
-    echo " 廬 "
-else
-    echo " "
-fi
--- scripts/utils/toggle_dunst.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/usr/bin/notify-send -i ~/.config/moji.png -t 3 "MOJI" "ive toogled dunst" && /usr/bin/notify-send "DUNST_COMMAND_TOGGLE" && /usr/bin/dunstctl history-pop
