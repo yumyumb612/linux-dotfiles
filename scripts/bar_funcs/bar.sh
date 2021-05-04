6e46b7c89d1f98600150c773fca0bf885571c243
yum <yumyum_b612@protonmail.ch>
2021-05-04 15:17:31 -0400

    Delete scripts directory

--- scripts/bar_funcs/bar.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-echo $(~/.SYSTEM/scripts/bar_funcs/battery.sh) "" $(~/.SYSTEM/scripts/bar_funcs/volume.sh) "" $(~/.SYSTEM/scripts/bar_funcs/brightness.sh) 
--- scripts/utils/trayer/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-$(/usr/bin/trayer --edge top --align center --SetDockType false --SetPartialStrut true --expand true --width 3 --transparent true --alpha 0 --tint 0x22222222 --height 16 --padding 6)
