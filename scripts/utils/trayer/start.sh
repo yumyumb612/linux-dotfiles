8469d507cf12f3a1b22557559474dc6be1551c09
kshly <yumyum_b612@protonmail.ch>
2021-11-03 09:51:05 -0400

    Delete scripts directory

--- scripts/utils/trayer/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-$(/usr/bin/trayer --edge bottom --align right --SetDockType true --SetPartialStrut true --expand true --width 2 --transparent true --alpha 0 --tint 0x202027 --height 20 --padding 5)
